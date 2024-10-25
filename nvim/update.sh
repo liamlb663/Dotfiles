0839f5bed0b378dff7a15fddd1b75a6749d0916e
Liam Ahearn <liamlb663@gmail.com>
2024-10-25 18:37:28 -0400

    Picom and Neovim

--- nvim/update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git pull
-
-head -n55 ~/.config/nvim/init.lua | tail -n50 > ~/.config/nvim/setup_conf.lua
-nvim --headless -u ~/.config/nvim/setup_conf.lua "+PackerInstall"
-rm -f ~/.config/nvim/setup_conf.lua
