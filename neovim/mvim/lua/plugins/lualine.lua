fbb027e01f57f1d01970b8963bfe8fcfc23ef23a
Liam Ahearn <liamlb663@gmail.com>
2025-02-26 15:01:13 -0500

    lualine with word count + markdown

---
+++ neovim/mvim/lua/plugins/lualine.lua
@@ -0,0 +1,56 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        local function wordcount()
+            return tostring(vim.fn.wordcount().words) .. ' words'
+        end
+
+        require('lualine').setup({
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+
+                -- Separators
+                component_separators = { left = '', right = ''},
+                section_separators = { left = '', right = ''},
+                -- component_separators = '',
+                -- section_separators = { left = '', right = '' },
+
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = false,
+                refresh = {
+                      statusline = 100,
+                      tabline = 100,
+                      winbar = 100,
+                }
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress', wordcount},
+                lualine_z = {'location'}
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        })
+    end
+}
---
+++ neovim/mvim/lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {{
+        "OXY2DEV/markview.nvim",
+        lazy = false
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = {
+            "MarkdownPreviewToggle",
+            "MarkdownPreview",
+            "MarkdownPreviewStop"
+        },
+        ft = { "markdown" },
+        config = function() vim.fn["mkdp#util#install"]() end,
+    }
+}
