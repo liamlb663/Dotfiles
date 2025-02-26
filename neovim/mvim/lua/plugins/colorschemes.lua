7f2b48eefcd0443919c7c9b3edf8cce1beeec342
Liam Ahearn <liamlb663@gmail.com>
2025-02-26 11:27:25 -0500

    New E-Ink colorschemes

--- neovim/mvim/lua/plugins/colorschemes.lua
+++ neovim/mvim/lua/plugins/colorschemes.lua
@@ -11,5 +11,15 @@ return {
         "ellisonleao/gruvbox.nvim",
         lazy = false,
         priority = 1000 ,
+    },
+    {
+        "alexxGmZ/e-ink.nvim",
+        priority = 1000,
+        config = function ()
+            require("e-ink").setup()
+            --vim.cmd.colorscheme "e-ink"
+
+            vim.opt.background = "dark"
+        end
     }
 }
--- neovim/nvim/lua/plugins/colorschemes.lua
+++ neovim/nvim/lua/plugins/colorschemes.lua
@@ -9,7 +9,17 @@ return {
         lazy = false,
         priority = 1000 ,
         config = function()
-            vim.cmd([[colorscheme gruvbox]])
+            vim.cmd.colorscheme "gruvbox"
+        end
+    },
+    {
+        "alexxGmZ/e-ink.nvim",
+        priority = 1000,
+        config = function ()
+            require("e-ink").setup()
+            --vim.cmd.colorscheme "e-ink"
+
+            vim.opt.background = "dark"
         end
     }
 }
