6c91cada414cc68a0f74ca7fb673fd4e058b3a12
Liam Ahearn <liam663lb@gmail.com>
2025-10-31 00:20:34 -0400

    leap fix

--- neovim/nvim/lua/plugins/textEditing.lua
+++ neovim/nvim/lua/plugins/textEditing.lua
@@ -7,8 +7,9 @@ return {
         "ggandor/leap.nvim",
         event = "BufReadPost",
         config = function()
-            local leap = require("leap")
-            leap.add_default_mappings()
+            --local leap = require("leap")
+
+            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
         end,
     },
     {
